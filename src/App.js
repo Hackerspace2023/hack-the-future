f04a2ca43a1a4d47e8e923bed6196aead70084cb
Aniruddha Ghosh <ghoshaniruddha2003@gmail.com>
2023-06-21 15:45:19 +0530

    About Page done

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import './App.css';
+import About from "./components/About/about";
 
 function App() {
     return (
         <div className="App">
             <h1>Hello World</h1>
+            <About/>
         </div>
     );
 }
