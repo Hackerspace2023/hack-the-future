f7b0535ebd81391cca464af245a4b5ab13b2b460
Aniruddha Ghosh <ghoshaniruddha2003@gmail.com>
2023-06-23 12:49:54 +0530

    Sponser Page redigned

--- src/components/sponsored/sponsored.jsx
+++ src/components/sponsored/sponsored.jsx
@@ -1,60 +1,35 @@
 import React from "react";
 import SponserList from "./SponserList";
 import './sponsored.css';
+
 const Sponser = () => {
-    const sponserData = [
-        {
-            id: 'sp1',
-            sponserName: 'Title Sponsor',
-            sponserWesite: 'https://iocl.com/',
-            sponserImage: "images/iocl.png",
-        }, {
-            id: 'sp2',
-            sponserName: 'International Partner',
-            sponserWesite: 'https://girlswhocode.com/',
-            sponserImage: 'images/gwc.png',
-        }, {
-            id: 'sp3',
-            sponserName: 'CoPowered by',
-            sponserWesite: 'https://www.codingninjas.in/',
-            sponserImage: 'images/cn.png',
-        }, {
-            id: 'sp4',
-            sponserName: 'Media Partner',
-            sponserWesite: 'https://www.jagran.com/',
-            sponserImage: 'images/dainikjagran.png',
-        }, {
-            id: 'sp5',
-            sponserName: 'Associate Sponsors',
-            sponserWesite: 'https://www.jetbrains.com/',
-            sponserImage: 'images/jetbrains.png',
-        }, {
-            id: 'sp6',
-            sponserName: 'Platform Partners',
-            sponserWesite: 'https://codechef.com/',
-            sponserImage: 'images/codechef.png',
-        },
-    ]
-    return (
-        <div>
-            <div id="lgx-sponsors" className="lgx-sponsors">
-                <div className="lgx-inner-bg">
-                    <div className="lgx-inner">
-                        <div className="container">
-                            <div className="row">
-                                <div className="col-xs-12">
-                                    <div className="lgx-heading">
-                                        <h2 className="heading">Offcial Sponsors</h2>
-                                        <h3 className="subheading">Memory provided by Paridhi'23 Sponsors!</h3>
-                                    </div>
-                                </div>
-                            </div>
-                            <SponserList items={sponserData}/>
-                        </div>
-                    </div>
-                </div>
-            </div>
-        </div>
-    )
+    const sponserData = [{
+        id: 'sp1', sponserWebsite: '#', sponserImage: "Images/mcdonalds-black-logo.png",
+    }, {
+        id: 'sp2', sponserWebsite: '#', sponserImage: 'Images/starbucks-logo-black-and-white.png',
+    }, {
+        id: 'sp3', sponserWebsite: '#', sponserImage: 'Images/general-electric-black-logo-png-transparent.png',
+    }, {
+        id: 'sp4', sponserWebsite: '#', sponserImage: 'Images/nfl-logo-png-transparent.png',
+    }, {
+        id: 'sp5', sponserWebsite: '#', sponserImage: 'Images/mercedes-benz-6-logo-png-transparent.png',
+    }, {
+        id: 'sp6', sponserWebsite: '#', sponserImage: 'Images/hogwarts-logo-png-transparent.png',
+    }, {
+        id: 'sp7', sponserWebsite: '#', sponserImage: "Images/mcdonalds-black-logo.png",
+    }, {
+        id: 'sp8', sponserWebsite: '#', sponserImage: 'Images/starbucks-logo-black-and-white.png'
+    }, {
+        id: 'sp9', sponserWebsite: '#', sponserImage: 'Images/general-electric-black-logo-png-transparent.png',
+    }, {
+        id: 'sp10', sponserWebsite: '#', sponserImage: 'Images/nfl-logo-png-transparent.png'
+    }, {
+        id: 'sp11', sponserWebsite: '#', sponserImage: 'Images/mercedes-benz-6-logo-png-transparent.png',
+    }, {
+        id: 'sp12', sponserWebsite: '#', sponserImage: 'Images/hogwarts-logo-png-transparent.png'
+    },]
+    return (<div className='slider'>
+        <SponserList items={sponserData}/>
+    </div>)
 };
 export default Sponser;
