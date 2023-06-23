f4e1f6472d0e85bda29d75e077b16f688f9036d8
Arnab Manna <107208391+arnab-4@users.noreply.github.com>
2023-06-23 20:34:27 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,9 +1,14 @@
 import './App.css';
+import Theme from "./components/Theme/theme.js"
+import Judge from "./components/Judges/judges.jsx"
 
 function App() {
 
     return (
         <div className="App">
+
+        <Theme />
+        <Judges />
 
         </div>
     );
