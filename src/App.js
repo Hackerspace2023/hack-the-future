6205f85233ad89f17026d05d19c80c1df03ebba6
samratghosh291 <samratghosh490@gmail.com>
2023-06-23 20:28:53 +0530

    update App.js

--- src/App.js
+++ src/App.js
@@ -1,13 +1,12 @@
 import './App.css';
-import Theme from "./components/Theme/theme.js"
-import Judge from "./components/Judges/judges.jsx"
+// import Theme from "./components/Theme/theme.js"
+// import Judge from "./components/Judges/judges.jsx"
 
 function App() {
 
     return (
         <div className="App">
-            <Theme/>
-            <Judge/>
+           
 
         </div>
     );
