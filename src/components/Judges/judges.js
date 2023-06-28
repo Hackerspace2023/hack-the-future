56f23da9a40ef6320fb6005216c279e94f613d89
Shekhar Hans <117727599+ShekharHans@users.noreply.github.com>
2023-06-28 19:06:28 +0530

    Update judges.js

--- src/components/Judges/judges.js
+++ src/components/Judges/judges.js
@@ -1,133 +1,104 @@
-import React from "react";
-import ReactCardCarousel from "react-card-carousel";
+import React, { useState, useEffect } from 'react';
+import './home.css'; // Import the CSS file for styling
 
-import "./judges.css"
-import Judge1 from "./assets/img1.jpg"
-import Judge2 from "./assets/img2.jpg"
-import Judge3 from "./assets/img3.jpg"
-import Judge4 from "./assets/img4.jpg"
-import Judge5 from "./assets/img5.jpg"
-import Judge6 from "./assets/img6.jpg"
+const Home = () => {
+    // const hackathonDate = new Date('2023-07-28');
+
+    const [timeLeft, setTimeLeft] = useState(calculateTimeLeft());
+
+    useEffect(() => {
+        const timer = setInterval(() => {
+            setTimeLeft(calculateTimeLeft());
+        }, 1000);
+
+        return () => {
+            clearInterval(timer);
+        };
+    }, []);
+
+    function calculateTimeLeft() {
+        const currentTime = new Date();
+        const targetDate = new Date('2023-07-27');
+        targetDate.setHours(23, 59, 59); // Set the target time to 11:59 pm
+
+        const timeDiff = targetDate.getTime() - currentTime.getTime();
+
+        if (timeDiff <= 0) {
+            return {
+                days: 0,
+                hours: 0,
+                minutes: 0,
+                seconds: 0,
+            };
+        }
+
+        const days = Math.floor(timeDiff / (1000 * 60 * 60 * 24));
+        const hours = Math.floor((timeDiff / (1000 * 60 * 60)) % 24);
+        const minutes = Math.floor((timeDiff / 1000 / 60) % 60);
+        const seconds = Math.floor((timeDiff / 1000) % 60);
+
+        return {
+            days,
+            hours,
+            minutes,
+            seconds,
+        };
+    }
 
 
-const Judges = () => {
-  const CONTAINER_STYLE = {
-    position: "relative",
-    height: "100vh",
-    width: "100%",
-    display: "flex",
-    flex: 1,
-    justifyContent: "center",
-    alignItems: "center",
-    overflow: "hidden",
-    margin: "10px"
+    return (
+        <div className="home-container" id="Home">
 
-  };
-
-  const CARD_STYLE = {
-    height: "300px",
-    width: "300px",
-    textAlign: "center",
-    background: "#eee",
-    color: "#000",
-    fontFamily: "sans-serif",
-    fontSize: "14px",
-    textTransform: "uppercase",
-    borderRadius: "3px",
-    margin: "0 200px",
-    boxSizing: "border-box"
-  };
-
-  // Array of card data
-  const cardData = [
-
-    {
-      name: "Jhon Doe",
-      image: Judge1,
-      desc: "Lorem ipsum dolor sit amet, consectetur adipisicing elit et cupiditate deleniti.",
-      social: [
-        { name: "LinkedIn", icon: "fa fa-linkedin" },
-      ]
-    },
-    {
-      name: "Jhon Doe",
-      image: Judge2,
-      desc: "Lorem ipsum dolor sit amet, consectetur adipisicing elit et cupiditate deleniti.",
-      social: [
-        { name: "LinkedIn", icon: "fa fa-linkedin" },
-      ]
-    },
-    {
-      name: "Jhon Doe",
-      image: Judge3,
-      desc: "Lorem ipsum dolor sit amet, consectetur adipisicing elit et cupiditate deleniti.",
-      social: [
-        { name: "LinkedIn", icon: "fa fa-linkedin" },
-      ]
-    },
-    {
-      name: "Jhon Doe",
-      image: Judge4,
-      desc: "Lorem ipsum dolor sit amet, consectetur adipisicing elit et cupiditate deleniti.",
-      social: [
-        { name: "LinkedIn", icon: "fa fa-linkedin" },
-      ]
-    },
-    {
-      name: "Jhon Doe",
-      image: Judge5,
-      desc: "Lorem ipsum dolor sit amet, consectetur adipisicing elit et cupiditate deleniti.",
-      social: [
-        { name: "LinkedIn", icon: "fa fa-linkedin" },
-      ]
-    },
-    {
-      name: "Jhon Doe",
-      image: Judge6,
-      desc: "Lorem ipsum dolor sit amet, consectetur adipisicing elit et cupiditate deleniti.",
-      social: [
-        { name: "LinkedIn", icon: "fa fa-linkedin" },
-      ]
-    },
-    
-
-    // Add more card data objects as needed
-  ];
-
-  return (
-    <div style={CONTAINER_STYLE}>
-      <ReactCardCarousel
-        autoplay={true}
-        autoplay_speed={2500}
-        spread={"wide"}
-        disable_box_shadow={true}
-      >
-        {cardData.map((card, index) => (
-          <div key={index} style={CARD_STYLE}>
-            <div className="card-one">
-              <header>
-                <div className="avatar">
-                  <img src={card.image} alt={card.name} />
-                </div>
-              </header>
-
-              <h3>{card.name}</h3>
-              <div className="desc">{card.desc}</div>
-              <div className="contacts">
-                {/* Render social icons */}
-                {card.social.map((social, index) => (
-                  <a key={index} href="">
-                    <i className={social.icon}></i>
-                  </a>
-                ))}
-                <div className="clear"></div>
-              </div>
+            <div className="title-container">
+                <h2 className="hackathon-title">Hack <span>The</span> Future</h2>
             </div>
-          </div>
-        ))}
-      </ReactCardCarousel>
-    </div>
-  );
+            <div className="section">
+                <div className="left-section">
+                    <h3 class="date"><b class="lgx-counter">28</b> <span>July</span></h3>
+                    <div className="timer">
+                        <div className="timer-item">
+                            <div className="timer-item-value">{timeLeft.days}</div>
+                            <div className="timer-item-label">Days</div>
+                        </div>
+                        <div className="timer-item">
+                            <div className="timer-item-value">{timeLeft.hours}</div>
+                            <div className="timer-item-label">Hours</div>
+                        </div>
+                        <div className="timer-item">
+                            <div className="timer-item-value">{timeLeft.minutes}</div>
+                            <div className="timer-item-label">Minutes</div>
+                        </div>
+                        <div className="timer-item">
+                            <div className="timer-item-value">{timeLeft.seconds}</div>
+                            <div className="timer-item-label">Seconds</div>
+                        </div>
+                    </div>
+                    <div className="button">
+                        <button className="btn btn-2 hover-slide-right register-btn">
+                            <span>Register Now  <i className="register fa-solid fa-arrow-right"></i></span>
+                        </button>
+                        <button className="btn btn-2 hover-slide-right discord">
+                            <a href="https://discord.gg/q9vHfn9M">Join Here <i className="fa-brands fa-discord"></i></a>
+                        </button>
+                    </div>
+                </div>
+                <div className="right-section">
+
+                    <h1 className='hackerspace'>&#x23;HackerSpace</h1>
+                    <h1 className='greetings'>Welcome to Hackathon <span className='span1'>2</span><span className='span2'>0</span><span className='span3'>2</span><span className='span4'>3</span>!</h1>
+                    <p className='home-text'>"Unlock your potential, solve challenges, and leave your mark at our Hackerthon event."</p>
+                    <h3 className='college-name'>
+                        <a href="/">
+                            <span>
+                                <i class="fa-solid fa-location-dot"></i>
+                            </span>
+                            Meghnad Saha Institute of Technology
+                        </a>
+                    </h3>
+                </div>
+            </div>
+        </div >
+    );
 };
 
-export default Judges;
+export default Home;
